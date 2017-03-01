15b2b5179fde9314d8082ea6b1a9e7ea67a1d7d9
Shane McConnell <transform.interactive@gmail.com>
2017-03-01 05:08:40 -0500

    Version 5.0
    Online Jan/Feb 2017

---
+++ calculator.js
@@ -0,0 +1,402 @@
+<!--hide script from older browsers
+//
+var AppVER = 5.0
+
+function hideDiv(id) {
+	if (document.getElementById) { // DOM3 = IE5, NS6
+		document.getElementById(id).style.display = 'none';
+	}
+} // end function
+
+function showDiv(id) {
+	if (document.getElementById) { // DOM3 = IE5, NS6
+		document.getElementById(id).style.display = 'block';
+	}
+} // end function
+
+function detaxation(Amt) {
+   var pretax_amt = (Amt)*(100 / (100 + (100 * 0.05)));
+   var prov_tax = 0; //Amt - (pretax_amt)
+   return prov_tax;
+} // end function
+
+function PROV_taxation(Amt,rate) {
+   var prov_tax = Amt*rate;
+   return prov_tax;
+ } // end function
+
+function RoundMoneyTwoDecimals(n) {
+	pennies = n * 100;
+	pennies = Math.round(pennies);
+	strPennies = "" + pennies;
+	len = strPennies.length;
+    //return strPennies.substring(0, len - 2) + "." + strPennies.substring(len - 2, len);
+    //result = Math.round(n*100)/100;
+	result = Math.round(n);
+	  // let's also add comments
+	  return addCommas(result.toFixed(2));
+} // end function
+
+function RoundUPMoneyTwoDecimals(n) {
+  pennies = n * 100;
+  pennies = Math.round(pennies);
+  strPennies = "" + pennies;
+  len = strPennies.length;
+    //return strPennies.substring(0, len - 2) + "." + strPennies.substring(len - 2, len);
+  result = Math.round(n*100)/100;
+	  // let's also add comments
+	return addCommas(result.toFixed(2));
+} // end function
+
+function OntarioRebate(inData,Buyer,ScndBuyer,OldBuyer) {
+	var inDataDecimal = inData - 0;
+
+	if ( Buyer || ScndBuyer)   {
+		// check if either is FTHB
+		if ( Buyer && ScndBuyer) {
+			ratio = 1; // both FTHB 100% Rebate
+		} else if ( OldBuyer || Buyer == 0 ) {
+			ratio = 0.5; // only one FTHB
+		}else{
+			ratio = 1; // one FTHB
+		}
+
+	  if ( inDataDecimal <= 55000 ) {
+			Rebate = (0.005 * inDataDecimal);
+		}else if (inDataDecimal <= 250000 ){
+			Rebate = ((0.01 * inDataDecimal) - 275);
+		}else if (inDataDecimal <= 400000 ){
+			Rebate = ((0.015 * inDataDecimal) - 1525);
+		}else if (inDataDecimal <= 2000000 ){
+			Rebate = ((0.02 * inDataDecimal) - 3525);
+		}else {
+			Rebate = 4000;
+		}
+		if (Rebate > 4000) Rebate = 4000;
+
+		return Rebate * ratio;
+
+	} else {
+		return 0;
+	}
+
+} // end function
+
+function TransferTax(inData) {
+	var inDataDecimal = inData -0;
+
+	if( inData == '' ) {
+		return 0.0;
+	}
+
+	if ( inDataDecimal <= 55000 ) {
+			return 0.005 * inDataDecimal;
+		}else if (inDataDecimal <= 250000 ){
+			return (0.01 * inDataDecimal) - 275
+		}else if (inDataDecimal <= 400000 ){
+			return (0.015 * inDataDecimal) - 1525
+		}else{
+			return (0.02 * inDataDecimal) - 3525
+		}
+
+} // end function
+
+function TorontoRebate(inData,Buyer,ScndBuyer,OldBuyer) {
+	var inDataDecimal = inData - 0;
+
+	if ( Buyer || ScndBuyer )   {
+
+		if ( Buyer && ScndBuyer ) {
+			ratio = 1;
+		} else if ( OldBuyer || Buyer == 0 ) {
+			ratio = 0.5;
+		} else {
+			ratio = 1;
+		}
+
+	  if ( inDataDecimal <= 55000 ) {
+			Rebate = (0.005 * inDataDecimal);
+		}else if (inDataDecimal <= 400000 ){
+			Rebate = ((0.01 * inDataDecimal) - 275);
+		}else {
+			Rebate = 3725;
+		}
+		if (Rebate > 3725) Rebate = 3725;
+
+		return Rebate * ratio;
+
+	} else {
+		return 0;
+	}
+
+} // end function
+
+function TorontoTransferTax(inData) {
+	var inDataDecimal = inData -0;
+	if( inData == '' ) {
+		return 0.0;
+	}
+
+		if ( inDataDecimal <= 55000 ) {
+			return 0.005 * inDataDecimal;
+		}else if (inDataDecimal <= 400000 ){
+			return (0.01 * (inDataDecimal-55000)) + 275;
+		}else {
+			return (0.02 * (inDataDecimal-400000)) + 3725;
+		}
+} // end function
+
+function set_fees(form,transaction) {
+
+	LegalFees = document.getElementById('fees');
+ 	LTT = document.getElementById('landTax');
+	TLTT = document.getElementById('TorontolandTax');
+	Costs = document.getElementById('totalClosingCost');
+	CostsInfo = document.getElementById('ClosingCostCalcs');
+	Buyer = document.getElementById('NewBuyer');
+	ScndBuyer = document.getElementById('NewBuyer2');
+	OldBuyer = document.getElementById('OldBuyer2');
+
+	if (document.calc_form.NewBuyer.checked == true)	BuyerStat = 1; else BuyerStat = 0;
+	if (document.calc_form.NewBuyer2.checked == true)	Buyer2Stat = 1; else Buyer2Stat = 0;
+	if (document.calc_form.OldBuyer2.checked == true)	OldBuyerStat = 1; else OldBuyerStat = 0;
+
+	P=form.price.value;
+	var objRegExp = /[$$,]/g;
+	P = eval(P.replace(objRegExp,''));
+
+	if (form.price.value.length == 0) P = "0";
+
+	// after July 1st -- calculate HST
+	PROV_rate = "13% HST";
+	rate = 0.13;
+	//alert (PROV_rate);
+
+	for (ic=0;ic<2;ic++) {
+		if (form.R3[ic].checked) gta_located=ic+1;
+	}
+
+	switch(gta_located){
+		case 1:
+			// in GTA
+			GTA = 1;
+		; break
+		case 2:
+			// outside GTA
+			GTA = 0;
+		; break
+	} // end switch
+
+	 for (ic=0;ic<3;ic++) {
+		if (form.R1[ic].checked) transaction=ic+1;
+ 	}
+
+	switch(transaction) {
+		case 1:
+			// purchase
+			showDiv('ValueRow');
+			showDiv('Rebates');
+
+			if (P == '') P = 0;
+			BaseFee = 1865;
+			F1 = (0.001*(P-500000));
+			if (F1 >= 0 ) {
+				Fees = BaseFee + F1;
+			}else{
+				Fees = BaseFee;
+			}
+
+		 	PROV_Tax = PROV_taxation(Fees,rate);
+		 	//alert (PROV_Tax);
+
+		  L = TransferTax(P);
+		  ONRebate = OntarioRebate(P,BuyerStat,Buyer2Stat,OldBuyerStat);
+		 	//alert (ONRebate);
+
+			if (GTA) {
+				TL = TorontoTransferTax(P);
+				TORebate = TorontoRebate(P,BuyerStat,Buyer2Stat,OldBuyerStat);
+				//alert (TORebate);
+			}else{
+				TL = 0;
+				TORebate = 0;
+			} // end if GTA
+
+			if (P<15000) {
+				TLTTAF = 0;
+				TLINFO = '';
+			}else{
+			 	TLTTAF = 84.75;
+			 	TLINFO = " (*incl Admin Fee)";
+			} // end < $15000
+
+			TCC = Fees + PROV_Tax + L - ONRebate + TL - TORebate + TLTTAF;
+			//alert (TCC);
+
+			if (ONRebate) {
+				RebateInfo = "  <i>(after $" + addCommas(ONRebate.toFixed(0)) +" rebate)</i>  ";
+			}else{
+			 	RebateInfo = '';
+			}
+
+			if (TORebate) {
+				TORebateInfo = "  <i>(after $" + addCommas(TORebate.toFixed(0)) +" rebate)</i> "; // + TLINFO;
+				hideDiv('TLAFINF');
+			}else {
+				TORebateInfo = '' + TLINFO;
+				showDiv('TLAFINF');
+			}
+
+			TotalFees = PROV_Tax + Fees;
+			// LegalFees.innerHTML = "$" + addCommas(PreTaxFees) + " = $" + RoundMoneyTwoDecimals(Fees) + " + 5% GST";
+			LegalFees.innerHTML =  "$" + RoundMoneyTwoDecimals(Fees) + " + " + PROV_rate + " = $"+ RoundMoneyTwoDecimals(TotalFees);
+			if(GTA){
+			 	TLTT.innerHTML = "$"+RoundMoneyTwoDecimals(TL-TORebate+TLTTAF) + TORebateInfo;
+			}else{
+			 	TLTT.innerHTML = "Not Applicable";
+			}
+			LTT.innerHTML = "$"+RoundMoneyTwoDecimals(L-ONRebate) + RebateInfo;
+			Costs.innerHTML = "$" + RoundMoneyTwoDecimals(TCC);
+			CostsInfo.innerHTML = "<font color='#FFFFFF'>" + TCC.toFixed(2) +" = "+ BaseFee +" + "+ F1 +" + "+ PROV_Tax.toFixed(2) +" + "+ L +" - "+ ONRebate +" + "+ TL +" - "+ TORebate +" + "+ TLTTAF +"</font>";
+
+		; break
+
+	case 2:
+	// sell
+		showDiv('ValueRow');
+		hideDiv('Rebates');
+		hideDiv('TLAFINF');
+
+		BaseFee = 1187;
+		F1 = (1*(P-500000))/1000;
+	 	if (F1 >= 0 ) {
+	 		Fees = BaseFee + F1;
+	 	}else{
+	 		Fees = BaseFee;
+	 	}
+
+	  L = 0.00;
+		ONRebate = 0.00;
+		TL = 0.00;
+		TORebate = 0.00;
+
+		PROV_Tax = PROV_taxation(Fees,rate);
+		TCC = Fees + PROV_Tax;
+
+		TotalFees = PROV_Tax + Fees;
+//		LegalFees.innerHTML = "$" + addCommas(PreTaxFees) + " = $" + RoundMoneyTwoDecimals(Fees) + " + 5% GST";
+		LegalFees.innerHTML =  "$" + RoundMoneyTwoDecimals(Fees) + " + " + PROV_rate + " = $"+ RoundMoneyTwoDecimals(TotalFees);
+
+		LTT.innerHTML = "Buyer pays tax";
+		TLTT.innerHTML = "Buyer pays tax";
+		Costs.innerHTML = "$" + RoundMoneyTwoDecimals(TCC);
+		CostsInfo.innerHTML = "<font color='#FFFFFF'>" + TCC.toFixed(2) +" = "+ BaseFee +" + "+ F1 +" + "+ PROV_Tax.toFixed(2) +"</font>";
+
+		; break
+
+	case 3:
+		// refinance
+		showDiv('ValueRow');
+		hideDiv('TLAFINF');
+
+
+		//  Fees = 1132.08+67.92;
+		//  Fees = 1200.00;
+		//    Fees = 1061.95+138.05;
+		// OCT 2016
+		//  Fees = 1200.00+156.00;
+
+		BaseFee = 1200.00
+		F1 = (1*(P-500000))/1000;
+		if (F1 >= 0 ) {
+			Fees = BaseFee + F1;
+		}else{
+			Fees = BaseFee;
+		}
+
+		PROV_Tax = PROV_taxation(Fees,rate);
+		PreTaxFees = RoundMoneyTwoDecimals(Fees - PROV_Tax);
+
+		L = 0.00;
+		TL = 0.00;
+		TCC = Fees + PROV_Tax;
+
+		// hideDiv('ValueRow');
+		hideDiv('Rebates');
+
+		TotalFees = PROV_Tax + Fees;
+		// LegalFees.innerHTML = "$" + addCommas(PreTaxFees) + " = $" + addCommas(Fees) + " + 13% HST";
+		LegalFees.innerHTML = "$" + addCommas(Fees) + " + " + PROV_rate + " = $"+ RoundMoneyTwoDecimals(TotalFees);
+		LTT.innerHTML = "Not Applicable";
+		TLTT.innerHTML = "Not Applicable";
+		Costs.innerHTML = "$" + RoundMoneyTwoDecimals(TCC);
+		CostsInfo.innerHTML = "<font color='#FFFFFF'>" + TCC.toFixed(2) +" = "+ BaseFee +" + "+ F1 +" + "+ PROV_Tax.toFixed(2) +"</font>";
+		; break
+
+	  } // end switch
+} // end set fees function
+
+function payment(form)   {
+  set_fees(form);
+} // end function
+
+// CLEAR ALL INPUT FIELDS
+function clearForm(form){
+
+	form.price.value="";
+
+	form.R1[0].checked = true;
+	form.R2[0].checked = true;
+	form.R3[0].checked = true;
+
+	form.NewBuyer.checked = false;
+	form.NewBuyer2.checked = false;
+	form.OldBuyer2.checked = false;
+
+	LTT = document.getElementById('landTax');
+	TLTT = document.getElementById('TorontolandTax');
+	Costs = document.getElementById('totalClosingCost');
+	CostsInfo = document.getElementById('ClosingCostCalcs');
+
+	LTT.innerHTML = "$0.00";
+	TLTT.innerHTML = "$0.00";
+	Costs.innerHTML = "$0.00";
+	CostsInfo.innerHTML = "<font color='#FAFAFA'> QuoteCalc V"+AppVER+" &copy;2017 </font>";
+
+	 form.R1[0].checked = true;
+	 form.R2[0].checked = true;
+	 form.R3[0].checked = true;
+
+	 form.NewBuyer.checked = false;
+	 form.NewBuyer2.checked = false;
+	 form.OldBuyer2.checked = false;
+
+	 LTT = document.getElementById('landTax');
+	 TLTT = document.getElementById('TorontolandTax');
+	 Costs = document.getElementById('totalClosingCost');
+
+	 LTT.innerHTML = "$0.00";
+	 TLTT.innerHTML = "$0.00";
+	 Costs.innerHTML = "$0.00";
+
+	 showDiv('ValueRow');
+	 showDiv('Rebates');
+	 hideDiv('TLAFINF');
+
+
+} // end function
+
+// comma-format numbers
+function addCommas(nStr) {
+	nStr += '';
+	x = nStr.split('.');
+	x1 = x[0];
+	x2 = x.length > 1 ? '.' + x[1] : '';
+	var rgx = /(\d+)(\d{3})/;
+	while (rgx.test(x1)) {
+		x1 = x1.replace(rgx, '$1' + ',' + '$2');
+	}
+	return x1 + x2;
+} // end function
+
+// end script hiding -->
--- quote.tpl
+++ quote.tpl
@@ -41,10 +41,10 @@ Or, if you have some questions and would like to hear more, please <a href="http
 <div class="formBlock">
 <input id="gta" value="L1" name="R3" onclick="payment(this.form);" checked="checked" type="radio"> <label for="gta">In Toronto</label><br>
 <input id="ont" value="L2" name="R3" onclick="payment(this.form);" type="radio"> <label for="ont">Outside of Toronto</label><br>
- 
+
  </div>
 
- 
+
 <h1 class="plan">Select your type of transaction</h1>
 
  <div class="formBlock">
@@ -52,84 +52,78 @@ Or, if you have some questions and would like to hear more, please <a href="http
  <input id="sell" value="V2" name="R1" onclick="set_fees(this.form,'2');payment(this.form);" type="radio"> <label for="sell">Sell</label><br>
  <input id="mortgageRefinance" value="V3" name="R1" onclick="set_fees(this.form,'3');payment(this.form);" type="radio"> <label for="mortgageRefinance">Mortgage Refinance</label>
  </div>
- 
+
 	<div style="display: block;" id="ValueRow">
 		<h1 class="house">Enter the price of your property</h1>
 		<div class="formBlock">
         <label for="price">Price of Property</label>
-		<input value="0" class="priceinput" name="price" id="price" onkeyup="payment(this.form);" type="text" size="20"> 
+		<input value="0" class="priceinput" name="price" id="price" onkeyup="payment(this.form);" type="text" size="20">
         </div>
 	</div>
-    
+
     <div style="display: block;" id="Rebates">
-  
+
 		<h1 class="checkoffs">Choose what best describes this transaction</h1>
-	    
+
         <div class="formBlock">
-        	<label>
-	    <input class="checkbox" id="firstTime" name="SFR" value="ON" onclick="payment(this.form);" type="checkbox">
-      	The property is a Single-family residence (detached or semi-detached home, townhome or condominium)</label><br>
+
 	    <label>
 	    <input class="checkbox" id="firstTime" name="NewBuyer" value="ON" onclick="payment(this.form);" type="checkbox">
-		I am a First Time Home Buyer</label><br>
-	    <label>
-	    <input class="checkbox" id="firstTime" name="CanPR" value="ON" onclick="payment(this.form);" type="checkbox">
-		I am a Canadian citizen or permanent resident (or become one within the next 18-months)</label><br>
-  			
+		I am a First Time Home Buyer </label><br>
+
 		<label>
 	    <input class="checkbox" id="coPurchase" name="NewBuyer2" value="ON" onclick="form.OldBuyer2.checked = false; payment(this.form);" type="checkbox">
 	    My Co-Purchaser is a First Time Home Buyer</label><br>
+
 	    <label>
 	    <input class="checkbox" id="coPurchase2" name="OldBuyer2" value="ON" onclick="form.NewBuyer2.checked = false; payment(this.form);" type="checkbox">
 	    My Co-Purchaser previously owned a home (anywhere)</label><br>
-		<label>
-	    <input class="checkbox" id="firstTime" name="CanPRBuyer2" value="ON" onclick="payment(this.form);" type="checkbox">
-		My Co-Purchaser is a Canadian citizen or permanent residents (or will become one within the next 18-months)</label><br>
-
-    
 		</div>
+
+	</div>
 	<!--//rebates-->
-  
-      <h1 class="calcs">Legal and Land Transfer Costs</h1>
-      <div class="formBlock">
-      <p class="calcLabel">Legal Fees (All Inclusive) <span id="fees">$1,650 
-      + 13% HST</span></p><br>
-      <p class="calcLabel">Your Toronto Land Transfer Tax will be <span class="calcinput" id="TorontolandTax">$0.00</span></p><b><font size="1">* Effective April 1, 2016 for more info please see the <a href="http://www1.toronto.ca/wps/portal/contentonly?vgnextoid=769f4b3d0e673510VgnVCM10000071d60f89RCRD&vgnextchannel=4f90ff0e43db1410VgnVCM10000071d60f89RCRD#3" target="_new">City of Toronto's website</a></font></b><br>
-      <br>
-      <p class="calcLabel">Your Ontario Land Transfer Tax will be <span class="calcinput" id="landTax">$0.00</span></p>
-      <br>
-      </div>
 
-      <h1 class="dollars">Final Closing Costs</h1>    
-      <div class="formBlock">
-      <p class="calcLabel">Total Closing Costs: <span class="calcinput" id="totalClosingCost">$1,650.00</span></p>
-      <b><font size="1">* this fee assumes no unforeseen complications or 
-      circumstances arise.</font></b><br>
-      <span id="ClosingCostCalcs"></span>
-	  </div>
+	<h1 class="calcs">Legal and Land Transfer Costs</h1>
+		<div class="formBlock">
+			<p class="calcLabel">Legal Fees (All Inclusive) <span id="fees">$1,865 + 13% HST</span></p>
+			<p class="calcLabel">Your Toronto Land Transfer Tax will be <span class="calcinput" id="TorontolandTax">$0.00</span></p>
+				<div style="display: block" id="TLAFINF">
+					<b><font size="1">* Effective April 1, 2016 for more info please see the <a href="http://www1.toronto.ca/wps/portal/contentonly?vgnextoid=769f4b3d0e673510VgnVCM10000071d60f89RCRD&vgnextchannel=4f90ff0e43db1410VgnVCM10000071d60f89RCRD#3" target="_new">City of Toronto's website</a></font></b>
+				</div>
+				<!--//tlafinf-->
 
-	
-	<input name="" class="clearButton" onclick="clearForm(this.form); return false;" src="images/butRestart.jpg" alt="Restart Calculator" type="image" width="111" height="24">
-	
+			<p class="calcLabel">Your Ontario Land Transfer Tax will be <span class="calcinput" id="landTax">$0.00</span></p>
+
+ 		</div>
+
+	<h1 class="dollars">Final Closing Costs</h1>
+		<div class="formBlock">
+			<p class="calcLabel">Total Closing Costs: <span class="calcinput" id="totalClosingCost">$2,107.00</span></p>
+			<b><font size="1">* this fee assumes no unforeseen complications or circumstances arise.</font></b><br>
+			<span id="ClosingCostCalcs">&nbsp;</span>
+		</div>
+
+<input name="" class="clearButton" onclick="clearForm(this.form); return false;" src="images/butRestart.jpg" alt="Restart Calculator" type="image" width="111" height="24">
+
 <p>&nbsp;</p>
-	
+
 </form>
-				
+
 
 </div>
 <!--//articles-->
-             
+
 			</div>
 <!--//mainLeft-->
             <div id="mainRight">
-            	
-              
-            
-            {include file="$doc_root/inc/tile.contact.inc.tpl"}    
-                
+
+
+
+            {include file="$doc_root/inc/tile.contact.inc.tpl"}
+
           	</div>
       		<!--//main right-->
-            
+
             {include file="$doc_root/inc/body.footer.inc.tpl"}
 <script type="text/javascript">
 var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
